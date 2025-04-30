416954e7386a249e75b054a24e4b81b7711b9675
JManuelLorenzo <juanma.lorenzo@correo.ucu.edu.uy>
2025-04-30 11:23:51 -0300

    ut4_PD3

---
+++ TareasPorUnidad/UT3/Ejercicios Extras/test/src/App.java
@@ -0,0 +1,8 @@
+import java.util.LinkedList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        LinkedList ListaNueva = new LinkedList<>();
+        int i = 0;
+    }
+}
