b2e261968de8721aa3ef719a0c6cd49de90bf93d
JManuelLorenzo <juanma.lorenzo@correo.ucu.edu.uy>
2025-06-12 10:51:57 -0300

    PD1

---
+++ TareasPorUnidad/UT6/ut6_pd1-faltatestingYConclusion/src/main/java/pd1/TNodoTrie.java
@@ -0,0 +1,123 @@
+package pd1;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class TNodoTrie implements INodoTrie {
+
+    private   TreeMap<Character, TNodoTrie> mapa;
+    private boolean esPalabra;
+    
+
+    public TNodoTrie() {
+        mapa = new TreeMap<>();
+        esPalabra = false;
+    }
+
+    @Override
+    public void insertar(String unaPalabra) {
+        TNodoTrie nodo = this;
+        if (unaPalabra.isEmpty()) {
+            nodo.esPalabra = true;
+            return;
+        } 
+        
+        
+        char caracter = unaPalabra.charAt(0);
+            if (mapa.get(caracter) == null) {
+                TNodoTrie nodoHijo = new TNodoTrie();
+                mapa.put(caracter, nodoHijo);
+                nodoHijo.insertar(unaPalabra.substring(1));
+                
+            }
+            else{
+                nodo = mapa.get(caracter);
+                nodo.insertar(unaPalabra.substring(1));
+
+            }
+       
+        }
+
+    private void imprimir(String s, TNodoTrie nodo) {
+        if (nodo != null) {
+            if (nodo.esPalabra) {
+                System.out.println(s);
+            }
+            Set<Entry<Character, TNodoTrie>> valores = nodo.mapa.entrySet();
+            for (Entry<Character,TNodoTrie> entry : valores) {
+                entry.getValue().imprimir(s + entry.getKey(), entry.getValue() );
+            }
+        }
+    }
+
+    @Override
+    public void imprimir() {
+        imprimir("", this);
+    }
+
+    public TNodoTrie buscarNodoTrie(String s) {
+        TNodoTrie nodo = this;
+        if (s.isEmpty()) {
+            return nodo;
+        }
+       {
+            TNodoTrie next  = nodo.mapa.get(s.charAt(0)); 
+            if (next != null){
+                return next.buscarNodoTrie(s.substring(1));
+            }
+            return null;
+        }
+    }
+
+    public void predecir(String prefijo, LinkedList<String> palabras) {
+   
+        if (esPalabra) {
+            palabras.add(prefijo);
+        }
+        
+        if (!prefijo.isEmpty()) {
+            Character c = prefijo.charAt(0);
+            prefijo = prefijo.substring(1);
+            if(mapa.get(c) != null){
+            mapa.get(c).predecir(prefijo, palabras);
+            }
+        }
+        else{
+            auxPredecir(prefijo, palabras, this);
+        }
+        }
+    private void auxPredecir(String prefijo, LinkedList<String> palabras, TNodoTrie nodo) {
+       if(nodo.esPalabra){
+        palabras.add(prefijo);
+       } 
+      Set<Entry<Character, TNodoTrie>> set = nodo.mapa.entrySet();
+      for (Entry<Character,TNodoTrie> entry : set) {
+        auxPredecir(prefijo + entry.getKey(), palabras, entry.getValue());
+      }
+    }
+
+    @Override
+public int buscar(String s) {
+    TNodoTrie nodo = this;
+    int comparaciones = 0;
+    while (!s.isEmpty()) {
+        TNodoTrie next = nodo.mapa.get(s.charAt(0));
+        if ( next != null) {
+        nodo = next;
+        comparaciones++;
+        s = s.substring(1);}
+        else{
+            return 0;
+        }
+    }
+    return nodo.esPalabra ? comparaciones : 0; // Asegura que estemos al final de algo con sentido. 
+}
+
+}
