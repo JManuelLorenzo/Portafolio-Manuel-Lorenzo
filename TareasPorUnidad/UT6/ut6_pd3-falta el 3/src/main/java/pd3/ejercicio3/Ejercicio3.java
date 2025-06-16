22e33e62e1be530294cab9b72c2b6e88d05cd721
JManuelLorenzo <juanma.lorenzo@correo.ucu.edu.uy>
2025-06-16 01:01:23 -0300

    agrego el ejercicio 3 del pd3 ut6

--- TareasPorUnidad/UT6/ut6_pd3-falta el 3/src/main/java/pd3/ejercicio3/Ejercicio3.java
+++ TareasPorUnidad/UT6/ut6_pd3-falta el 3/src/main/java/pd3/ejercicio3/Ejercicio3.java
@@ -2,7 +2,13 @@
 package pd3.ejercicio3;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.TreeMap;
+
 
 /*
 Escribe  un  programa  que  lea  cadenas  de  caracteres  de  entrada  y  las  imprima  ordenadas  según  su  longitud, 
@@ -12,16 +18,46 @@ lexicográfico.
 
  // Creo que la solucion es un diccionario de int y AVL? Creando un comparable maybe?
 public class Ejercicio3 {
-   /*  public static void Ejercicio3(){
-        TreeMap<Integer,String>
-        HashMap<Integer, Trie> HashMap = new HashMap<>();
-        HashMap.put(1, "a");
-        HashMap.put(2, "ab");
-        HashMap.put(3, "abc");
-        HashMap.put(2, "ba");
-        System.out.println(HashMap);
-    }
     public static void main(String[] args) {
-        Ejercicio3();
-    }*/
+        
+    
+   TreeMap<String, Integer> stringMap = new TreeMap<>();
+   TreeMap<Integer,String> integerMap = new TreeMap<>();
+    boolean flag = true;
+    Scanner scanner = new Scanner(System.in);
+    while (flag) {
+        String palabraPorInsertar = scanner.nextLine();
+        if (palabraPorInsertar.equals("-")) {
+            flag = false;
+            break;
+        }
+        stringMap.put(palabraPorInsertar, palabraPorInsertar.length());
+    }
+
+
+   
+   /// Insertar las palabras en el string map en base a la cantidad de length que tenga cada uno.
+   
+   // El iterator usa el comparator, por String.
+   Set<Entry<String,Integer>>setString = stringMap.entrySet();
+   Iterator<Entry<String,Integer>> iteradorString  = setString.iterator();
+   while(iteradorString.hasNext()){
+       Entry<String, Integer> entrada  = iteradorString.next();
+       String str = entrada.getKey();
+       Integer num  = entrada.getValue();
+       if (integerMap.containsKey(num)) { // En el caso de que esta dentro del otro diccionario.
+        String temp = integerMap.get(num) + "\n" + str;
+        integerMap.put(num, temp);
+       }
+       else{
+        integerMap.put(num, str); // Sino esta lo ponemos dentro.
+       }
+   }
+   Set<Entry<Integer, String>>setInteger = integerMap.entrySet();
+   Iterator<Entry<Integer, String>> iteradorNum  = setInteger.iterator();
+   while (iteradorNum.hasNext()) {
+    System.out.println(iteradorNum.next().getValue());
+   }
+}
+ 
 }
