c66c828e6f1b7b7a73cdd68df218e228dc1145ea
JManuelLorenzo <juanma.lorenzo@correo.ucu.edu.uy>
2025-06-16 13:14:52 -0300

    Aplicacion de autocompletar de ut6_pd5

---
+++ TareasPorUnidad/UT6/ut6_pd5/src/main/java/pd5/AplicacionAutoCompletar.java
@@ -0,0 +1,30 @@
+package pd5;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class AplicacionAutoCompletar {
+   public static void main(String[] args) throws IOException {
+BufferedReader br = new BufferedReader(new FileReader("src/main/java/pd5/texto.txt")); 
+String str;
+LinkedList<String> lista = new LinkedList<>();
+TArbolTrie arbolTrie = new TArbolTrie();
+
+while ((str = br.readLine()) != null) {
+   String[] palabras = str.split(" ");
+   for (String string : palabras) {
+      string = string.toLowerCase().replaceAll("[^\\p{L}]", "");
+      arbolTrie.insertar(string);
+   }
+   
+}
+lista = arbolTrie.predecir("a");
+for (String string : lista) {
+   System.out.println(string);
+}
+   }
+   
+}
+
---
+++ TareasPorUnidad/UT6/ut6_pd5/src/main/java/pd5/TArbolTrie.java
@@ -0,0 +1,43 @@
+package pd5;
+
+import java.util.LinkedList;
+
+public class TArbolTrie implements IArbolTrie {
+
+    private TNodoTrie raiz;
+
+    @Override
+    public void insertar(String palabra) {
+        if (palabra == null || palabra.isEmpty()) {
+            return;
+        }
+        if (raiz == null) {
+            raiz = new TNodoTrie();
+        }
+        raiz.insertar(palabra);
+    }
+
+    @Override
+    public void imprimir() {
+        if (raiz != null) {
+            raiz.imprimir();
+        }
+    }
+
+    @Override
+    public int buscar(String palabra) {
+        if (raiz == null) {
+            return 0;
+        }
+        return raiz.buscar(palabra);
+    }
+
+    @Override
+    public LinkedList<String> predecir(String prefijo) {
+        LinkedList<String> palabras = new LinkedList<>();
+        if (raiz != null) {
+            raiz.predecir(prefijo, palabras);
+        }
+        return palabras;
+    }
+}
