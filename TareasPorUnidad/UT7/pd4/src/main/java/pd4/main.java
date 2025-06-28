878ba27d79e167f5dc674e0f2f1ac7489362bdb3
JManuelLorenzo <juanma.lorenzo@correo.ucu.edu.uy>
2025-06-28 18:56:01 -0300

    push

--- TareasPorUnidad/UT7/pd4/src/main/java/pd4/main.java
+++ TareasPorUnidad/UT7/pd4/src/main/java/pd4/main.java
@@ -11,6 +11,8 @@ public class main
 {
     public static void main( String[] args )
     {
+        // Pequeño test en base a comprobacion de archivo creado a mano.
+        
         pd4.tdas.TGrafoDirigido a = UtilGrafos.cargarGrafo("pd4/src/main/java/pd4/tdas/textos/aeropuertos.txt", "pd4/src/main/java/pd4/tdas/textos/conexiones.txt", false, pd4.tdas.TGrafoDirigido.class);
         for(TCamino camino : a.todosLosCaminos("Montevideo", "Curitiba").getCaminos()){
             System.out.println("Un camino es: " + camino.imprimirEtiquetas() + " " + camino.getCostoTotal());
---
+++ TareasPorUnidad/UT7/pd5/src/main/java/pd5/Topologia/respuesta.md
@@ -0,0 +1,7 @@
+### PseudoCódigo:
+esta la jpg correspondiente.
+
+
+### Respuesta preguntas:
+¿Cómo harías para obtener todas las ordenaciones topológicas existentes? 
+Se deberia implementar un sistema que nos permite algo similar a todos los caminos, lo que nos permitieria obtener los elementos, usando backtracking.  
